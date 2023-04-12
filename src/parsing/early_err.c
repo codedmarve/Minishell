1037aa456d913114f3ea6080e19b6ff576bb0603
Daria Goremykina <d.goremykina@gmail.com>
2023-04-12 13:14:29 +0200

    some cleaning

--- src/parsing/early_err.c
+++ src/parsing/early_err.c
@@ -9,15 +9,11 @@ int	early_err(t_data *data)
 	char	*input;
 
 	input = data->input;
-	if (spaces_err(input) == -1)
-		return (-1);
-	if (quotes_err(input) == -1)
-		return (-1);
-	if (pipes_err(input) == -1)
-		return (-1);
-	if (spec_char_err(input) == -1)
-		return (-1);
-	if (redirection_err(input) == -1)
+	if ((spaces_err(input) == -1)
+		|| (quotes_err(input) == -1)
+		|| (pipes_err(input) == -1)
+		|| (spec_char_err(input) == -1)
+		|| (redirection_err(input) == -1))
 		return (-1);
 	return (0);
 }
