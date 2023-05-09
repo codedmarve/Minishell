322215f55da6d825e13f7fbc9e650a65464c2971
Daria Goremykina <d.goremykina@gmail.com>
2023-05-09 12:22:54 +0200

    moved to signal2.c

--- includes/minishell.h
+++ includes/minishell.h
@@ -165,19 +165,6 @@ void	token_add_back(t_token **lst, t_token *new);
 void	remove_quotes(char *s);
 int		find_end(char *str, char *possible_sep);
 
-// // 04expander.c
-// char	*expand_exit_status(char *exp, t_idx *idx);
-// char	*expand_env_var(char *exp, t_idx *idx, char *token, t_data *data);
-// char	*expand_single_char(char *exp, t_idx *idx, char c);
-// char	*expand_token(char *token, t_data *data);
-// void	expand_token_lst(t_data *data);
-
-// // 04expander_utils.c
-// int		dollar_in_str(char *s);
-// char	*get_exit_status(void);
-// int	ft_strcmp(char *s1, char *s2);
-// char	*find_envp_value(t_envp *env_lst, char *var_name);
-
 // 04expander_init1.c
 void	init_exit_status(char **new_ptr, t_idx *idx);
 void	init_single_dollar(char **new_ptr, t_idx *idx);
@@ -196,11 +183,11 @@ int		dollar_in_str(char *s);
 char	*get_exit_status(void);
 
 // // 04expander.c
-char	*expand_exit_status(char *exp, t_idx *idx);
-char	*expand_env_var(char *exp, t_idx *idx, char *token, t_data *data);
-char	*expand_single_char(char *exp, t_idx *idx, char c);
-char	*expand_token(char *token, t_data *data);
+// char	*expand_exit_status(char *exp, t_idx *idx);
+// char	*expand_env_var(char *exp, t_idx *idx, char *token, t_data *data);
+// char	*expand_single_char(char *exp, t_idx *idx, char c);
 // char	*expand_token(char *token, t_data *data);
+char	*expand_token(char *token, t_data *data);
 void	expand_token_lst(t_data *data);
 
 //signals1.c
--- src/parsing/04expander.c
+++ src/parsing/04expander.c
@@ -6,54 +6,53 @@
 /*   By: dgoremyk <dgoremyk@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 15:01:50 by dgoremyk          #+#    #+#             */
-/*   Updated: 2023/05/08 16:57:24 by dgoremyk         ###   ########.fr       */
+/*   Updated: 2023/05/09 12:22:12 by dgoremyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+// char	*expand_exit_status(char *exp, t_idx *idx)
+// {
+// 	char	*exit_status;
 
-char	*expand_exit_status(char *exp, t_idx *idx)
-{
-	char	*exit_status;
+// 	exit_status = get_exit_status();
+// 	if (!exit_status)
+// 		return (NULL);
+// 	ft_strcpy(&exp[idx->j], exit_status);
+// 	idx->j += ft_strlen(exit_status);
+// 	free(exit_status);
+// 	idx->i += 2;
+// 	return (exp);
+// }
 
-	exit_status = get_exit_status();
-	if (!exit_status)
-		return (NULL);
-	ft_strcpy(&exp[idx->j], exit_status);
-	idx->j += ft_strlen(exit_status);
-	free(exit_status);
-	idx->i += 2;
-	return (exp);
-}
+// char	*expand_env_var(char *exp, t_idx *idx, char *token, t_data *data)
+// {
+// 	char	var_name[MAX_TOKEN_SIZE];
+// 	int		var_len;
+// 	char	*var_value;
 
-char	*expand_env_var(char *exp, t_idx *idx, char *token, t_data *data)
-{
-	char	var_name[MAX_TOKEN_SIZE];
-	int		var_len;
-	char	*var_value;
+// 	var_len = 0;
+// 	while (ft_isalnum(token[idx->i + 1 + var_len]) || token[idx->i + 1 + var_len] == '_')
+// 		var_len++;
+// 	ft_strncpy(var_name, &token[idx->i + 1], var_len);
+// 	var_name[var_len] = '\0';
+// 	var_value = find_envp_value(data->env_lst, var_name);
+// 	if (var_value)
+// 	{
+// 		while (*var_value)
+// 			exp[idx->j++] = *var_value++;
+// 	}
+// 	idx->i += var_len + 1;
+// 	return (exp);
+// }
 
-	var_len = 0;
-	while (ft_isalnum(token[idx->i + 1 + var_len]) || token[idx->i + 1 + var_len] == '_')
-		var_len++;
-	ft_strncpy(var_name, &token[idx->i + 1], var_len);
-	var_name[var_len] = '\0';
-	var_value = find_envp_value(data->env_lst, var_name);
-	if (var_value)
-	{
-		while (*var_value)
-			exp[idx->j++] = *var_value++;
-	}
-	idx->i += var_len + 1;
-	return (exp);
-}
-
-char	*expand_single_char(char *exp, t_idx *idx, char c)
-{
-	exp[idx->j++] = c;
-	idx->i++;
-	return (exp);
-}
+// char	*expand_single_char(char *exp, t_idx *idx, char c)
+// {
+// 	exp[idx->j++] = c;
+// 	idx->i++;
+// 	return (exp);
+// }
 
 // char	*expand_token(char *token, t_data *data)
 // {
